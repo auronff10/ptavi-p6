9302c78e33e57a2c9647a134972f8355369722fc
auronff10 <ledesmax@gmail.com>
2014-11-18 19:49:01 +0100

    Definidos los metodos que se envian segun que se recibe, se incluye el como enviar el audio

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
+import os
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -19,7 +21,15 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
-
+            method = line.split(" ")[0]
+            if method == "INVITE":
+                self.wfile.write("SIP/2.0 100 Trying\r\n")
+                self.wfile.write("SIP/2.0 180 Ring\r\n")
+                self.wfile.write("SIP/2.0 200 OK\r\n")
+            elif method == "ACK":
+                aEjecutar = ('mp32rtp -i ' + IP + ' -p 23032 < ' + fichero_audio)
+                print "Vamos a ejecutar", aEjecutar
+                os.system(aEjecutar) 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
